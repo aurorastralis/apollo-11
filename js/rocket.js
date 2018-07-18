c4960bc7b239c28c6377005edc0b008f0289f7e2
Aurora <aurorasw@live.com>
2018-07-18 09:05:24 +0100

    Car stuff and more thangs

---
+++ js/rocket.js
@@ -0,0 +1,34 @@
+var timer = null;
+var countdownNumber = 10
+
+var changeState = function (state) {
+	document.body.className = 'body-state' + state;
+
+clearInterval(timer);
+
+    if (state == 2) {
+    	timer = setInterval(function () {
+    		document.getElementById('countdown').innerHTML = countdownNumber;
+    		countdownNumber = countdownNumber-1;
+    		 	if (countdownNumber <= 0) {
+    		 	changeState(3);
+
+    		 	}
+
+    		 	else if (state == 3) {
+    		 		var success = setTimeout(function ()
+    		 		{
+    		 			var randomNumber = Math.random()
+    		 		}
+    		 			);
+    		 		}
+    		 	}
+
+    	}, 1000);
+    
+
+    };
+
+}
+ 
+
